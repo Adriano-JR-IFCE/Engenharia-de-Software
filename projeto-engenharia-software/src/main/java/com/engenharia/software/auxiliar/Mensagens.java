cd861fd51f3f8846672b8a254ea6db3fa6e5146c
WandersonGomes <dersom100@gmail.com>
2022-06-28 01:58:21 -0300

    Mensagem de excecao de preco do ingresso negativo.

--- projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
+++ projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
@@ -3,4 +3,5 @@ package com.engenharia.software.auxiliar;
 public class Mensagens {
     public final String EXCECAO_MENSAGEM_TITULO_EM_BRANCO = "Titulo do filme em branco!";
     public final String EXCECAO_MENSAGEM_QTD_ASSENTOS_NEGATIVA = "Quantidade de assentos negativa!";
+    public final String EXCECAO_MENSAGEM_PRECO_INGRESSO_NEGATIVO = "Preco negativo!";
 }
