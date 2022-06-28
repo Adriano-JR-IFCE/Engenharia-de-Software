d843e8ec6ec14df5f25da4929b6330d99cb1bbb5
WandersonGomes <dersom100@gmail.com>
2022-06-28 03:12:02 -0300

    Adicionada mais mensagens para a excecao de qtdAssentos negativa ou nula e a excecao de total negativo ou nulo.

--- projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
+++ projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
@@ -3,5 +3,7 @@ package com.engenharia.software.auxiliar;
 public class Mensagens {
     public final String EXCECAO_MENSAGEM_TITULO_EM_BRANCO = "Titulo do filme em branco!";
     public final String EXCECAO_MENSAGEM_QTD_ASSENTOS_NEGATIVA = "Quantidade de assentos negativa!";
+    public final String EXCECAO_MENSAGEM_QTD_ASSENTOS_NEGATIVA_OU_NULA = "Quantidade de assentos negativa ou nula!";
     public final String EXCECAO_MENSAGEM_PRECO_INGRESSO_NEGATIVO = "Preco negativo!";
+    public final String EXCECAO_MENSAGEM_TOTAL_NEGATIVO_OU_NULO = "Total negativo ou nulo!";
 }
