ddb9172fe7555deba236c76e7bd24450088849c4
WandersonGomes <dersom100@gmail.com>
2022-06-28 22:48:20 -0300

    Atualizar o nome da mensagem de excecao EXCECAO_MENSAGEM_PRECO_INGRESSO_NEGATIVO para EXCECAO_MENSAGEM_PRECO_INGRESSO_NULO_OU_NEGATIVO

--- projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
+++ projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
@@ -4,7 +4,7 @@ public class Mensagens {
     public final String EXCECAO_MENSAGEM_TITULO_EM_BRANCO = "Titulo do filme em branco!";
     public final String EXCECAO_MENSAGEM_QTD_ASSENTOS_NEGATIVA = "Quantidade de assentos negativa!";
     public final String EXCECAO_MENSAGEM_QTD_ASSENTOS_NEGATIVA_OU_NULA = "Quantidade de assentos negativa ou nula!";
-    public final String EXCECAO_MENSAGEM_PRECO_INGRESSO_NULO_OU_NEGATIVO = "Preco negativo!";
+    public final String EXCECAO_MENSAGEM_PRECO_INGRESSO_NULO_OU_NEGATIVO = "Preco nulo ou negativo!";
     public final String EXCECAO_MENSAGEM_TOTAL_NEGATIVO_OU_NULO = "Total negativo ou nulo!";
     public final String EXCECAO_MENSAGEM_ID_INVALIDO = "Id invalido!";
 }
