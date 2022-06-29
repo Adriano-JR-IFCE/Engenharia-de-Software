06a5a86f705cde77e4ca3bf235d812553721e294
WandersonGomes <dersom100@gmail.com>
2022-06-28 22:32:56 -0300

    Centralizou a tela do JFrameCRUDFilme e tambem adicionamos o botao CADASTRAR que abre um JDialogCadastrarFilme

--- projeto-engenharia-software/src/main/java/com/engenharia/software/gui/JFrameCRUDFilme.java
+++ projeto-engenharia-software/src/main/java/com/engenharia/software/gui/JFrameCRUDFilme.java
@@ -4,6 +4,10 @@
  */
 package com.engenharia.software.gui;
 
+import com.engenharia.software.controller.FilmeController;
+import com.engenharia.software.model.Filme;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lobonegro
@@ -15,6 +19,9 @@ public class JFrameCRUDFilme extends javax.swing.JFrame {
      */
     public JFrameCRUDFilme() {
         initComponents();
+        
+        //apresentar a tela no centro
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -31,6 +38,8 @@ public class JFrameCRUDFilme extends javax.swing.JFrame {
         comboTipoPesquisa = new javax.swing.JComboBox<>();
         inputPesquisa = new javax.swing.JTextField();
         btnPequisar = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        btnCadastrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Filmes");
@@ -42,6 +51,11 @@ public class JFrameCRUDFilme extends javax.swing.JFrame {
         comboTipoPesquisa.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Identificador", "Titulo", "Qtd. Assentos", "Preco Ingresso" }));
 
         btnPequisar.setText("PESQUISAR");
+        btnPequisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPequisarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -67,17 +81,48 @@ public class JFrameCRUDFilme extends javax.swing.JFrame {
                 .addContainerGap(16, Short.MAX_VALUE))
         );
 
+        jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        btnCadastrar.setText("CADASTRAR");
+        btnCadastrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadastrarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnCadastrar)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnCadastrar)
+                .addContainerGap(293, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 339, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -93,6 +138,38 @@ public class JFrameCRUDFilme extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPequisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPequisarActionPerformed
+        int indiceOpcaoEscolhida = comboTipoPesquisa.getSelectedIndex();
+        
+        switch (indiceOpcaoEscolhida) {
+            //identificador
+            case 0:
+                System.out.println("[LOG] indice da opcao escolhida: " + indiceOpcaoEscolhida);
+                
+                String entradaInputPesquisa = inputPesquisa.getText();
+                
+                try {
+                   Long id = Long.parseLong(entradaInputPesquisa);
+                   
+                   FilmeController filmeController = new FilmeController();
+                   Filme filme = filmeController.filme(id);
+
+                  System.out.println("[LOG] Filme encontrado:\n" + filme);
+                
+                  //CODIGO AQUI
+                  
+                } catch (Exception exception) {
+                   JOptionPane.showMessageDialog(this, "Error: " + exception.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
+                
+                break;
+        }
+    }//GEN-LAST:event_btnPequisarActionPerformed
+
+    private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
+        new JDialogCadastrarFilme(this, true).setVisible(true);        
+    }//GEN-LAST:event_btnCadastrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -130,10 +207,12 @@ public class JFrameCRUDFilme extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCadastrar;
     private javax.swing.JButton btnPequisar;
     private javax.swing.JComboBox<String> comboTipoPesquisa;
     private javax.swing.JTextField inputPesquisa;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
