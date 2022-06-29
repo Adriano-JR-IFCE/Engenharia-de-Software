45eb0516e1137d1b8fb9e1c84ddb8ea1c2ec58e6
WandersonGomes <dersom100@gmail.com>
2022-06-28 21:20:13 -0300

    Adicionada a mensagem de EXCECAO_MENSAGEM_ID_INVALIDO

--- projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
+++ projeto-engenharia-software/src/main/java/com/engenharia/software/auxiliar/Mensagens.java
@@ -6,4 +6,5 @@ public class Mensagens {
     public final String EXCECAO_MENSAGEM_QTD_ASSENTOS_NEGATIVA_OU_NULA = "Quantidade de assentos negativa ou nula!";
     public final String EXCECAO_MENSAGEM_PRECO_INGRESSO_NEGATIVO = "Preco negativo!";
     public final String EXCECAO_MENSAGEM_TOTAL_NEGATIVO_OU_NULO = "Total negativo ou nulo!";
+    public final String EXCECAO_MENSAGEM_ID_INVALIDO = "Id invalido!";
 }
